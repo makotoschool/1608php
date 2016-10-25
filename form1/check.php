4b8becde4cfe11f26202a9acedd6cbd01737c168
603teacer <goodbooks823@gmail.com>
2016-10-25 14:38:10 +0900

    まずcheck.phpに入力内容が出てくるように

--- form1/check.php
+++ form1/check.php
@@ -1,6 +1,7 @@
 <?php
 $name = htmlspecialchars($_POST['onamae'],ENT_QUOTES);
-
+$email = htmlspecialchars($_POST['e-mail'],ENT_QUOTES);
+$msg = htmlspecialchars($_POST['msg'],ENT_QUOTES);
 
 ?>
 <!DOCTYPE html>
@@ -15,7 +16,13 @@ $name = htmlspecialchars($_POST['onamae'],ENT_QUOTES);
 	<div class="content">
 		<h1>入力確認</h1>
 		<p>
-			あなたのお名前は<?php print $name; ?>ですね
+			あなたのお名前は<br><?php print $name; ?>ですね
+		</p>
+		<p>
+			あなたのe-mailは<br><?php print $email; ?><br>ですね
+		</p>
+		<p>
+			あなたの問い合わせ内容は<br><?php print $msg; ?><br>ですね
 		</p>
 	</div>
 </div>
