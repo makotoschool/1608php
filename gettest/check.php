14d93e833aa9aba57943eff220d3ae41f77e5177
hlamworks <hlam1030@yahoo.co.jp>
2016-11-04 11:22:52 +0900

    news onepage added

---
+++ gettest/check.php
@@ -0,0 +1,19 @@
+<?php
+$val = $_GET['data'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link type="text/css" rel="stylesheet" href="./css/main.css">
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
+<title>get送信チェック</title>
+</head>
+<body>
+<?php
+	echo $val; 
+
+?>
+</body>
+</html>
--- news_onepage/index.php
+++ news_onepage/index.php
@@ -1,13 +1,24 @@
 <?php
-if(isset($_GET['top_val'])){
-	$val=htmlspecialchars($_GET['top_val'],ENT_QUOTES);
 
-	$url='http://news.google.com/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss&topic='.$val.'&num=100';
-	$obj=simplexml_load_file($url);
+if(isset($_GET['news'])){
+$w=htmlspecialchars($_GET['news'],ENT_QUOTES);
+
+
 }
 
 
 
+//リクエストをする
+$request = 'http://news.google.com/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8
+&output=rss&topic='.$w;
+ 
+$result=simplexml_load_file($request);
+/*
+echo '<pre>';
+print_r($result);
+
+echo '</pre>';
+*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,32 +27,36 @@ if(isset($_GET['top_val'])){
 <link type="text/css" rel="stylesheet" href="./css/main.css">
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 <script type="text/javascript" src="./js/main.js"></script>
-<title>news onepage</title>
+<title>news</title>
 </head>
 <body>
 <div class="container">
-	<h1>ニュース</h1>
+	<h1>Googleニュース検索</h1>
 	<nav>
 		<ul>
-			<li><a href="index.php?top_val=ir">top</a></li>
-			<li><a href="index.php?top_val=y">社会</a></li>
-			<li><a href="index.php?top_val=s">スポーツ</a></li>
-
+			<li><a href="index.php?news=ir">top</a></li>
+			<li><a href="index.php?news=y">社会</a></li>
+			<li><a href="index.php?news=s">スポーツ</a></li>
+			<li><a href="index.php?news=e">エンタメ</a></li>
+			<li><a href="index.php?news=w">国際</a></li>
 		</ul>
-	</nav>	
+	</nav>
+	<div class="search">
+		<form action="result.php" method="POST">
+			<input type="text" name="word">
+			<input type="submit" value="トップニュースから検索する">	
+		</form>
+	</div>
 	<div class="content">
+		<h2>トップニュース</h2>
 		<?php
-			if(isset($_GET['top_val'])){
+			foreach($result->channel->item as $item):?>
+				<div class="box">
+				<a href="<?php echo $item->link ;?>" target="blank"><h2><?php echo $item->title; ?></h2></a>	
+				<p><?php echo $item->description; ?></p>
+				</div>
+			<?php endforeach; ?>
 
-				foreach($obj->channel->item as $item){
-					echo '<h2>'.$item->title.'</h2>';
-					echo '<p>'.$item->description.'</p>';
-
-				}
-			}
-
-		
-		?>
 	</div>
 </div>
 </body>
