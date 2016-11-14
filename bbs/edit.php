039c8740b215e5025d4c365e2e554df1ed008fe2
hlamworks <hlam1030@yahoo.co.jp>
2016-11-14 12:01:13 +0900

    編集のフォーム初期値

--- bbs/edit.php
+++ bbs/edit.php
@@ -2,7 +2,12 @@
 //GET送信があればの世界でくるむ＊そのままだとエラーが出る
 if($_SERVER['REQUEST_METHOD']==='GET'){
 $id=htmlspecialchars($_GET['editid'],ENT_QUOTES);
-print_r($id);
+//DB接続
+require_once(__DIR__.'./lib/dbh.php');
+//SQL発行と内容の取得(PDOインスタンスは$dbhに入っている)
+$stmt=$dbh->prepare('SELECT * FROM bbs1 WHERE id=?');//動的にSQL文を組み立てる時、ハッカー対策にプリぺアードステートメントの仕組みでSQL発行
+$stmt->execute(array($id));
+
 
 }
 ?>
@@ -18,17 +23,21 @@ print_r($id);
 <body>
 <div class="container">
 	<h1>投稿を編集する</h1>
+
 	<div class="content">
+	<?php foreach($stmt as $row):?>
 		<form action="" method="post">
 			<p>お名前<br>
-			<input name="name" type="text">
+			<input name="name" type="text" value="<?php echo $row['name'];?>">
 			</p>
 			<p>投稿内容<br>
 			<textarea name="msg"></textarea>
 			</p>
 			<input type="submit" value="投稿確認">	
 		</form>
+	<?php endforeach; ?>
 	</div>
+
 </div>
 </body>
 </html>
