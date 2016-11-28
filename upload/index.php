889f2ca22824d7fa70bd63de0902bc406c0ce7ca
hlamworks <hlam1030@yahoo.co.jp>
2016-11-28 12:32:15 +0900

    enctipe 追加

--- upload/index.php
+++ upload/index.php
@@ -1,3 +1,30 @@
+<?php
+if($_SERVER['REQUEST_METHOD']==='POST'){
+
+//チェックしたい
+//$error_msg='画像ファイルをアップしてください';
+//ファイルのサイズ（容量）
+if(isset($_FILES['up_file']['tmp_name'])){
+ echo 'うまくいってるよ';
+
+/*
+	if($_FILES['up_file']['size']>1024){
+		$error_msg='ファイルの上限は1Mです';
+
+	}
+
+*/
+}
+
+
+
+
+//ファイルの拡張子
+
+
+
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,11 +36,16 @@
 <div class="container">
 	<div class="content">
 		<h1>画像をアップロードしてください</h1>
-		<form action="index.php" method="post">
+		<form action="index.php" method="post" enctype="multipart/form-data">
 			<label for="title">タイトル</label><br>	
 			<input type="text" name="title" id="title"><br>
 			<label for="up_file">ファイルを選択してください</label><br>
 			<input type="file" name="up_file" id="up_file"><br>
+			<?php				
+				if(isset($error_msg)){
+					echo '<p>'.$error_msg.'</p>';
+				}
+			?>
 			<input type="submit" value="投稿する">
 
 
